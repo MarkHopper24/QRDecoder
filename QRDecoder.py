26007958681b5906f2edf01ec9f4529c17bb6795
MarkHopper24 <106134632+MarkHopper24@users.noreply.github.com>
2022-06-06 14:26:53 -0500

    Added clarity to comments.

--- QRDecoder.py
+++ QRDecoder.py
@@ -1,17 +1,17 @@
-from PIL import Image #Non-standard
+from PIL import Image
 from subprocess import Popen
-from PIL import ImageGrab #Non-standard
-from pyzbar.pyzbar import decode #Non-standard
+from PIL import ImageGrab
+from pyzbar.pyzbar import decode
 import os
 import time
 import tkinter as tk
-import sv_ttk #Non-standard
+import sv_ttk
 import psutil
 
 #Capturing script run timestamp
 programStart = time.time()
 
-#Function close out other existing instances of the same process
+#Function to close out other existing instances of the same process
 def closeIfProcessRunning(processName):
     #Loop through all running processes
     for proc in psutil.process_iter():
@@ -20,16 +20,16 @@ def closeIfProcessRunning(processName):
             if (processName.lower() in proc.name().lower()) and (proc.create_time() < programStart) and (proc.pid != os.getpid()):
                 #Terminate it
                 proc.terminate()
-            #If an exception is thrown, should be safe to assume no other instances. Moving on
+            #If an exception is thrown, should be safe to assume there are no other instances and move on.
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess, psutil.TimeoutExpired, psutil.Error):
             pass
 
 #Close other instances of QRDecoder.exe
 closeIfProcessRunning('QRDecoder.exe')
 
-#File for image captured with snipping tool
+#File for temp image captured by user with snipping tool
 capturedQR = 'temp.png'
-#Fake temp QR image if no image is detected in the user's clipboard when script is running
+#Fake generated temp image if no image is detected in the user's clipboard when script starts running
 fakeQR = 'placeholder.png'
 
 #Making sure temp files were cleared out from last run
@@ -38,11 +38,11 @@ if(os.path.exists(capturedQR) == True):
 if(os.path.exists(fakeQR) == True):
     os.remove(fakeQR)
 
-#Check user's clipboard for existing image, decode, and set it to starting clipboard contents
+#Check user's clipboard for to see if there's already an existing image there, decode it, and set it to starting clipboard contents
 try:
     startingClipboard = ImageGrab.grabclipboard()
     startingContents = list(startingClipboard.getdata())
-#If no image detected, set decoded clipboard contents to fake temp QR
+#If no image detected, set decoded clipboard contents to the fake generated temp image
 except:
     fakeImage = Image.new("RGB", size=(10, 10), color='black')
     fakeImage.save(fakeQR)
@@ -52,18 +52,19 @@ except:
 #Launch snipping tool
 Popen("start ms-screenclip:", shell=True)
 
-#Waiting for file creation which indicates clipboard change
+#Waiting for file creation which indicates clipboard change 
+#In short, when the file exists, we know the user's clipboard is different than when we started and can exit the loop.
 while os.path.exists(capturedQR) == False:
     #Checking for new clipboard contents
     try:
         newClipboards = ImageGrab.grabclipboard()
         newContents = list(newClipboards.getdata())
-    #Passing exception if user still doesn't have clipboard image
+    #Passing exception if user still doesn't have a clipboard image
     except:
         pass
     #Checking if new clipboard contents have been detected
     if('newContents' in locals()):
-        #Same contents, sleep and loop again
+        #Same contents as when we started, sleep and loop again
         if(newContents == startingContents):
             time.sleep(.1)
         #Success! New clipboard image detected. Save to a file that will exit the loop
@@ -93,7 +94,7 @@ field = tk.Text(root)
 field.insert(tk.INSERT, QRContents)
 field.pack(fill='both', expand=True)
 
-#Configure field to readonly
+#Configure field to read-only
 field.configure(state="disabled", selectbackground="Yellow",
                 selectforeground="Black")
 
@@ -104,7 +105,7 @@ x_cordinate = int((root.winfo_screenwidth() / 2) - (root.winfo_width() / 2))
 y_cordinate = int((root.winfo_screenheight() / 2) - (root.winfo_height() / 2))
 root.geometry("+{}+{}".format(x_cordinate, y_cordinate - 20))
 
-#Launch GUY
+#Launch GUI
 root.mainloop()
 
 #Cleanup files once GUI exits
