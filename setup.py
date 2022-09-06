f39c4dca64a2496a46126de75e9debb0b5a7944f
MarkHopper24 <106134632+MarkHopper24@users.noreply.github.com>
2022-09-05 19:39:13 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if sys.platform == "win32":
 
 setup(
     name="QR Decoder",
-    version="1.1",
+    version="1.2",
     options={"build_exe": build_exe_options},
     
     executables=[Executable(script='QRDecoder.py', base = base, icon='Logo.ico')]
